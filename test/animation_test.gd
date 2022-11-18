830b911203d600cda5c32d3a45f31bd31df2d72d
011eh <1395663435@qq.com>
2022-11-18 17:32:31 +0800

    测试——动画

--- test/animation_test.gd
+++ test/animation_test.gd
@@ -5,15 +5,9 @@ func _process(delta):
 		var animation = $AnimationPlayer.get_animation('run_down') as Animation
 		animation.track_set_path(0, "Skin:frame")
 		animation.track_set_key_value(0,0,1)
-		animation.track_set_key_value(0,1,2)
-		animation.track_set_key_value(0,2,3)
-		animation.track_set_key_value(0,3,4)
 		
 	if Input.is_action_pressed('ui_down'):
 		var animation = $AnimationPlayer.get_animation('run_down') as Animation
 		animation.track_set_path(0, "Skin:frame")
 		animation.track_set_key_value(0,0,4)
-		animation.track_set_key_value(0,1,5)
-		animation.track_set_key_value(0,2,6)
-		animation.track_set_key_value(0,3,7)
 		
