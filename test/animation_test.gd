9dde2bb3437bc2c3cf1815dc7b41ff61bf45783a
011eh <1395663435@qq.com>
2022-11-26 22:58:01 +0800

    测试——动画

--- test/animation_test.gd
+++ test/animation_test.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
+
+func _ready():
+	print(FileAccess.file_exists('res://player/idle_s3tate.gd'))
+
 func _process(delta):
 	if Input.is_action_pressed('ui_up'):
-		var animation = $AnimationPlayer.get_animation('run_down') as Animation
+		var animation = $AnimationPlayer.get_animation('run') as Animation
 		animation.track_set_path(0, "Skin:frame")
 		animation.track_set_key_value(0,0,1)
-		
+
 	if Input.is_action_pressed('ui_down'):
-		var animation = $AnimationPlayer.get_animation('run_down') as Animation
+		var animation = $AnimationPlayer.get_animation('run') as Animation
 		animation.track_set_path(0, "Skin:frame")
 		animation.track_set_key_value(0,0,4)
-		
+
