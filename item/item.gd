530354444d2fe30848a44a37b4756c3641c1e0e7
011eh <1395663435@qq.com>
2022-12-30 17:53:07 +0800

    物品

--- item/item.gd
+++ item/item.gd
@@ -9,6 +9,7 @@ const SOLT_ITEM_RES_TABLE := {
 	13: preload('res://item/res/state_item/edrink.tres'),
 	44: preload('res://item/res/state_item/mre_1.tres'),
 	72: preload('res://item/res/craft/pepper_seeds_pack.tres'),
+	82: preload('res://item/res/weapon/knife_butcher.tres'),
 }
 
 
