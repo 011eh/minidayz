9fec1fc619e30e392f4807b2840ea92da45a5bf0
011eh <1395663435@qq.com>
2023-01-06 17:57:23 +0800

    开发工具

--- dev_res/item_res_util.gd
+++ dev_res/item_res_util.gd
@@ -1,34 +1,29 @@
 extends Node2D
 
-
-const SLOT_ITEM_DIR = 'res://item/res/slot_item/'
-const STATE_ITEM_DIR = 'res://item/res/state_item/'
-const CRAFT_DIR = 'res://item/res/craft/'
-
 enum ItemType { SLOT_ITEM, STATE_ITEM, CRAFT}
 
+
+const res_dict := {
+	ItemType.SLOT_ITEM: 'res://item/res/slot_item/',
+	ItemType.STATE_ITEM: 'res://item/res/state_item/',
+	ItemType.CRAFT: 'res://item/res/craft/',
+}
+const file_dict := {
+	ItemType.SLOT_ITEM: 'res://dev_res/data/背包物品.CSV',
+	ItemType.STATE_ITEM: 'res://dev_res/data/状态物品.CSV',
+	ItemType.CRAFT: 'res://dev_res/data/工具.CSV',
+}
+
+
 func _ready():
-	print_res_dictionary(ItemType.SLOT_ITEM)
-	print_res_dictionary(ItemType.STATE_ITEM)
-	print_res_dictionary(ItemType.CRAFT)
+#	print_res_dictionary(ItemType.SLOT_ITEM)
+#	print_res_dictionary(ItemType.STATE_ITEM)
+#	print_res_dictionary(ItemType.CRAFT)
+	create_res_file_by_csv(ItemType.SLOT_ITEM)
 
 func create_res_file_by_csv(item_type: ItemType):
-	var file: FileAccess
-	var item: ItemResource
-
-	match item_type:
-		ItemType.SLOT_ITEM:
-			file = FileAccess.open('res://dev_res/背包物品.csv',FileAccess.READ)
-			item = SlotItemResource.new()
-
-		ItemType.STATE_ITEM:
-			file = FileAccess.open('res://dev_res/状态物品.csv',FileAccess.READ)
-			item = StateItemResource.new()
-
-		ItemType.CRAFT:
-			file = FileAccess.open('res://dev_res/工具.csv',FileAccess.READ)
-			item = CraftResource.new()
-
+	var file := FileAccess.open(file_dict.get(item_type), FileAccess.READ)
+	var item := SlotItemResource.new()
 	var properties := file.get_csv_line()
 	while not file.eof_reached():
 		var values := file.get_csv_line()
@@ -42,30 +37,11 @@ func create_res_file_by_csv(item_type: ItemType):
 				item.set(properties[i], value)
 			
 			var file_name := item.item_name.to_lower().replace(' ','_')
-			if item is SlotItemResource and not item is StateItemResource:
-				var path := '%s%s.tres' % [SLOT_ITEM_DIR ,file_name]
-				ResourceSaver.save(item, path)
-			
-			if item is StateItemResource:
-				var path := '%s%s.tres' % [STATE_ITEM_DIR ,file_name]
-				ResourceSaver.save(item, path)
-			
-			if item is CraftResource:
-				var path := '%s%s.tres' % [CRAFT_DIR ,file_name]
-				ResourceSaver.save(item, path)
+			var path := '%s%s.tres' % [res_dict.get(item_type) ,file_name]
+			ResourceSaver.save(item, path)
 
 func print_res_dictionary(type: ItemType):
-	var dir: String
-	match type:
-		ItemType.SLOT_ITEM:
-			dir = SLOT_ITEM_DIR
-			
-		ItemType.STATE_ITEM:
-			dir = STATE_ITEM_DIR
-			
-		ItemType.CRAFT:
-			dir = CRAFT_DIR
-
+	var dir := res_dict.get(type) as String
 	var file_names := DirAccess.get_files_at(dir)
 	if file_names.size() > 0:
 		var dict := {}
@@ -79,4 +55,3 @@ func print_res_dictionary(type: ItemType):
 		for key in keys:
 			print('%d: preload(\'%s\'),' % [key, dict[key]])
 		print('}')
-		print()
