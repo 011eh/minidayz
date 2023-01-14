b0632dea0d1398c33116d8593bb6517a6fff2111
011eh <1395663435@qq.com>
2023-01-14 23:29:34 +0800

    测试

--- test/item_list.gd
+++ test/item_list.gd
@@ -1,10 +1,29 @@
-extends Node2D
+extends HSplitContainer
 
 
+@export
+var gui_texture: Texture2D
+@export
+var frame_size: int
+@onready
+var item_list := $ItemList as ItemList
+@onready
+var label := $RichTextLabel as RichTextLabel
+var id_dict := {}
 func _ready():
-	var count := 0
-	var ids: PackedInt32Array
-#	add_child(SlotItem.create_item(47))
+	var h_frames := gui_texture.get_width() / frame_size
+	var keys := SlotItem.RES_TABLE.keys()
+	for i in range(keys.size()):
+		var id := keys[i] as int
+		var texture := AtlasTexture.new()
+		texture.atlas = gui_texture
+		texture.region = Rect2(
+			id % h_frames * frame_size,
+			id / h_frames * frame_size,
+			frame_size,
+			frame_size
+			)
+		item_list.add_icon_item(texture)
 
 #	ids = SlotItem.RES_TABLE.keys()
 #	for id in ids:
