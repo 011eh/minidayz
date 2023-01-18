59ba7a4a1762be2b8a0e9f74b129b9a575e3771b
011eh <1395663435@qq.com>
2023-01-18 17:53:05 +0800

    测试

--- test/test.gd
+++ test/test.gd
@@ -3,5 +3,30 @@ extends Node2D
 class_name Test
 
 
+var slots: Array[Gear]
+
+
 func _ready():
-	pass
+	var item: Item
+	for i in range(100):
+		item = ItemCreator.create_item(SlotItem, 0)
+		add_child(item)
+	
+	item = ItemCreator.create_item(SlotItem, 49)
+	add_child(item)
+	item = ItemCreator.create_item(MeleeWeapon, 0)
+	add_child(item)
+	item = ItemCreator.create_item(MainWeapon, 0)
+	add_child(item)
+	item = ItemCreator.create_item(Gear, 0)
+	add_child(item)
+	item = ItemCreator.create_item(Gear, 17)
+	add_child(item)
+	item = ItemCreator.create_item(Gear, 24)
+	add_child(item)
+	item = ItemCreator.create_item(Gear, 42)
+	add_child(item)
+	item = ItemCreator.create_item(Gear, 48)
+	add_child(item)
+	item = ItemCreator.create_item(Pistol, 25)
+	add_child(item)
