c07c756bdbceac2170cacd2f7e693aa81e72b59e
011eh <1395663435@qq.com>
2023-01-29 17:03:29 +0800

    人物背包，物品拾取、物品堆叠逻辑修改

--- character/inventory.gd
+++ character/inventory.gd
@@ -12,11 +12,6 @@ var equipment_slots: Array[Item]
 @onready
 var pickup_area := $PickupArea as Area2D
 
-
-#func _process(delta):
-#	for i in range(WEPAON_OFFSET):
-#		print(equipment_slots[i].slots)
-
 func _ready():
 	pickup_area.area_entered.connect(pickup)
 	equipment_slots.resize(EQUIPMENT_SLOT_NUMBER)
@@ -29,21 +24,39 @@ func pickup(area: Area2D) -> void:
 		var index := get_slot_index(item)
 		var item_in_equment_slot := equipment_slots[index]
 		if is_instance_valid(item_in_equment_slot):
-			# todo drop item
+			# todo，丢弃装备
 			pass
 		equipment_slots[index] = item
-		print(index)
+		item.get_parent().remove_child(item)
 		emit_signal('equipment_changed', index, item.texture)
-
 	else:
+		var gears: Array[Item]
+		if item is NumberItem:
+			gears = equipment_slots.slice(0, WEPAON_OFFSET) as Array[Gear]
+			var items:Array[NumberItem] = []
+			for gear in gears:
+				if is_instance_valid(gear):
+					var same_item := func(item_in_slot: NumberItem) -> NumberItem:
+						if item.get_item_id() == item_in_slot.resource.id:
+							return item_in_slot
+						else:
+							return null
+					items.append_array(gear.slots.map(same_item))
+			# todo，将物品堆叠到背包物品
+		
 		var not_full = func(gear: Gear) -> bool:
-			return is_instance_valid(gear) and gear.not_full()
-		var gears: Array[Item] = equipment_slots.slice(0, WEPAON_OFFSET).filter(not_full)
-		var sort_by_durability = func(g1: Gear, g2: Gear) -> bool:
-			return g1.durability > g2.durability
-		gears.sort_custom(sort_by_durability)
+			return is_instance_valid(gear) and gear.has_empty_slot()
+		gears = equipment_slots.slice(0, WEPAON_OFFSET).filter(not_full) as Array[Item]
+		var sort_by_stackable_and_durability = func(g1: Gear, g2: Gear) -> bool:
+			var item_id := item.get_item_id()
+			var g1_stackable := is_instance_valid(g1.get_item_to_stack(item_id))
+			var g2_stackable := is_instance_valid(g2.get_item_to_stack(item_id))
+			if g1_stackable == g1_stackable:
+				return g1.durability > g2.durability
+			return g1_stackable > g2_stackable
+		gears.sort_custom(sort_by_stackable_and_durability)
 		if not gears.is_empty():
-			gears.front().add_to_slot(item)
+			gears.pop_front().add_to_slot(item)
 			item.get_parent().remove_child(item)
 
 func get_slot_index(item: Item) -> int:
