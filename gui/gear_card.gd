157e32f8a70545fd07654f0a5244a4bc192aab59
011eh <1395663435@qq.com>
2023-02-06 20:17:42 +0800

    GUI修改

--- gui/gear_card.gd
+++ gui/gear_card.gd
@@ -3,11 +3,14 @@ extends TextureRect
 
 const SlotTexture := preload('res://asset/images/gui/inventory/gui_inv_slot_shadow.png')
 const GEAR_ICON_REGION := Rect2(0, 320, 32, 32)
-const DURABILITY_OFFSET := 11
+const DURABILITY_OFFSET := 8
 
 
 func _ready():
-	update(ItemCreator.create_item(Gear, 6))
+	var gear := ItemCreator.create_item(Gear, 6) as Gear
+	gear.durability = 34.34
+	update(gear)
+	pass
 
 func update(gear: Gear) -> void:
 	var resource := gear.resource
@@ -16,7 +19,7 @@ func update(gear: Gear) -> void:
 	atlas.region = GEAR_ICON_REGION
 	$Icon.texture = atlas
 	var name := $Name
-	name.text = 'fffffffffffffffffaa'
+	name.text = resource.item_name
 	$Durability.text = '%.0f%%' % gear.durability
 	if name.get_line_count() == 2:
 		$Durability.offset_top += DURABILITY_OFFSET
