1560ce4b70f1950effbcef5ee0af0052a5924b50
011eh <1395663435@qq.com>
2023-02-20 17:51:23 +0800

    测试

--- test/test.gd
+++ test/test.gd
@@ -2,14 +2,8 @@ extends Node2D
 
 
 func _ready():
-	var gear := ItemCreator.create_item(Gear, 18) as Gear
+	var gear := ItemCreator.create_item(Gear, 6) as Gear
 	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
 	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
 	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
-	$InventoryUI/Cards/ClothesCard.update_gear_ui(gear)
-	
-	gear = ItemCreator.create_item(Gear, 18) as Gear
-	gear.add_to_slot(ItemCreator.create_item(NumberItem, 31))
-	gear.add_to_slot(ItemCreator.create_item(NumberItem, 31))
-	gear.add_to_slot(ItemCreator.create_item(NumberItem, 31))
-	$InventoryUI/Cards/PantsCard.update_gear_ui(gear)
+	$InventoryUI.update_inventory_ui(1,gear)
