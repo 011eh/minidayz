e36fd897e7e49c99837169ef8c85f79ad247bdbb
011eh <1395663435@qq.com>
2023-02-21 18:01:19 +0800

    测试

--- test/test.gd
+++ test/test.gd
@@ -2,8 +2,20 @@ extends Node2D
 
 
 func _ready():
+	ui_test()
+
+
+func ui_test() -> void:
 	var gear := ItemCreator.create_item(Gear, 6) as Gear
 	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
 	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
 	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
-	$InventoryUI.update_inventory_ui(1,gear)
+	$InventoryUI.update_inventory_ui(PlayerInventory.PLAYER_SLOT, gear)
+	$InventoryUI.update_inventory_ui(PlayerInventory.CLOTHES, gear)
+	$InventoryUI.update_inventory_ui(PlayerInventory.PANTS, gear)
+	$InventoryUI.update_inventory_ui(PlayerInventory.VEST, gear)
+	$InventoryUI.update_inventory_ui(PlayerInventory.BACKPACK, gear)
+	$InventoryUI.update_inventory_ui(PlayerInventory.HELMET, ItemCreator.create_item(Gear, 25))
+	$InventoryUI.update_inventory_ui(PlayerInventory.MAIN_WEAPON, null)
+	$InventoryUI.update_inventory_ui(PlayerInventory.PISTOL, ItemCreator.create_item(Pistol, 26))
+	$InventoryUI.update_inventory_ui(PlayerInventory.MELEE_WEAPON, ItemCreator.create_item(MeleeWeapon, 2))
