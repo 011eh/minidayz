7fcab56a060046ea518ee1a8f88cd9ed56fa1a4e
011eh <1395663435@qq.com>
2023-02-22 18:08:16 +0800

    测试

--- test/pick_test.gd
+++ test/pick_test.gd
@@ -3,8 +3,38 @@ extends Node2D
 func _ready():
 	var inventory_ui := $CanvasLayer/InventoryUI
 	$Player/Inventory.slot_item_changed.connect(inventory_ui.update_inventory_ui)
-	stack_test()
-	pass
+	
+	var item: Item
+	for i in range(200):
+		item = ItemCreator.create_random(NumberItem)
+		$ItemPosition.position.x += 20
+		item.position = $ItemPosition.position
+		add_child(item)
+	
+	var equip: Item
+	for i in range(20):
+		equip = ItemCreator.create_random(Gear)
+		$EquipmentPosition.position.x += 20
+		equip.position = $EquipmentPosition.position
+		add_child(equip)
+	
+	for i in range(20):
+		equip = ItemCreator.create_random(Pistol)
+		$Pistol.position.x += 20
+		equip.position = $Pistol.position
+		add_child(equip)
+	
+	for i in range(20):
+		equip = ItemCreator.create_random(MainWeapon)
+		$MainWeapon.position.x += 20
+		equip.position = $MainWeapon.position
+		add_child(equip)
+	
+	for i in range(20):
+		equip = ItemCreator.create_random(MeleeWeapon)
+		$MeleeWeapon.position.x += 20
+		equip.position = $MeleeWeapon.position
+		add_child(equip)
 
 func stack_test():
 	var item: Item
@@ -43,50 +73,4 @@ func stack_test():
 	item = ItemCreator.create_item(Gear, 1)
 	$EquipmentPosition.position.x += 200
 	item.position = $EquipmentPosition.position
-	add_child(item)
-	
-
-func inventory_test():
-	var item: Item
-	for i in range(8):
-		item = ItemCreator.create_item(NumberItem, i)
-		$ItemPosition.position.x += 20
-		item.position = $ItemPosition.position
-		add_child(item)
-	
-	item = ItemCreator.create_item(NumberItem, 0)
-	$ItemPosition.position.x += 20
-	item.position = $ItemPosition.position
-	add_child(item)
-	
-	item = ItemCreator.create_item(Gear, 0)
-	item.position = $EquipmentPosition.position 
-	add_child(item)
-	item = ItemCreator.create_item(Gear, 17)
-
-	item.position = $EquipmentPosition.position 
-	add_child(item)
-	item = ItemCreator.create_item(Gear, 24)
-
-	item.position = $EquipmentPosition.position 
-	add_child(item)
-	item = ItemCreator.create_item(Gear, 42)
-
-	item.position = $EquipmentPosition.position 
-	add_child(item)
-	item = ItemCreator.create_item(Gear, 48)
-
-	item.position = $EquipmentPosition.position 
-	add_child(item)
-	item = ItemCreator.create_item(MainWeapon, 0)
-
-	item.position = $EquipmentPosition.position 
-	add_child(item)
-	item = ItemCreator.create_item(Pistol, 25)
-
-	item.position = $EquipmentPosition.position 
-	add_child(item)
-	item = ItemCreator.create_item(MeleeWeapon, 0)
-
-	item.position = $EquipmentPosition.position 
 	add_child(item)
