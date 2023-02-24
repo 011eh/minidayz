f4d739bff511f1b9dae3939222c95e55399b6b84
011eh <1395663435@qq.com>
2023-02-24 17:29:07 +0800

    测试

--- test/test.gd
+++ test/test.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 
+var array :=[NumberItem]
+
 func _ready():
-	$Items/ItemIcon.update_item_ui(ItemCreator.create_random(NumberItem))
-	$Items/ItemIcon2.update_item_ui(ItemCreator.create_random(NumberItem))
+	print(NumberItem)
+	print(array[0])
+	
+#	$Items/ItemIcon.update_item_ui(ItemCreator.create_random(NumberItem))
+#	$Items/ItemIcon2.update_item_ui(ItemCreator.create_random(NumberItem))
+	ui_test()
 
 
 func ui_test() -> void:
@@ -19,15 +25,14 @@ func ui_test() -> void:
 	$InventoryUI.update_inventory_ui(PlayerInventory.PISTOL, null)
 	$InventoryUI.update_inventory_ui(PlayerInventory.MELEE_WEAPON, null)
 	
-	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
-	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
-	gear.add_to_slot(ItemCreator.create_item(Craft, 92))
+	gear.slots[1] = ItemCreator.create_item(Craft, 92) 
+	gear.slots[2] = ItemCreator.create_item(Knife, 11) 
 	$InventoryUI.update_inventory_ui(PlayerInventory.PLAYER_SLOT, gear)
 	$InventoryUI.update_inventory_ui(PlayerInventory.CLOTHES, gear)
 	$InventoryUI.update_inventory_ui(PlayerInventory.PANTS, gear)
 	$InventoryUI.update_inventory_ui(PlayerInventory.VEST, gear)
-	$InventoryUI.update_inventory_ui(PlayerInventory.BACKPACK, gear)
+	$InventoryUI.update_inventory_ui(PlayerInventory.BACKPACK, ItemCreator.create_item(Gear, 51))
 	$InventoryUI.update_inventory_ui(PlayerInventory.HELMET, ItemCreator.create_item(Gear, 25))
 	$InventoryUI.update_inventory_ui(PlayerInventory.MAIN_WEAPON, ItemCreator.create_item(MainWeapon, 2))
 	$InventoryUI.update_inventory_ui(PlayerInventory.PISTOL, ItemCreator.create_item(Pistol, 26))
-	$InventoryUI.update_inventory_ui(PlayerInventory.MELEE_WEAPON, ItemCreator.create_item(MeleeWeapon, 2))
+	$InventoryUI.update_inventory_ui(PlayerInventory.MELEE_WEAPON, null)
