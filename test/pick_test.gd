6f257fcbfcfc03d7e173db6105d903f19e511eb5
011eh <1395663435@qq.com>
2023-03-17 17:54:11 +0800

    测试

--- test/pick_test.gd
+++ test/pick_test.gd
@@ -1,39 +1,41 @@
 extends Node2D
 
+
 func _ready():
+	var inventory := $Player/Inventory as PlayerInventory
 	var inventory_ui := $CanvasLayer/InventoryUI
-	inventory_ui.setup($Player/Inventory)
-	
+	inventory_ui.setup(inventory)
+
 	for item in ItemCreator.create_all(NumberItem):
 		$NumberItem.position.x += 25
 		item.position = $NumberItem.position
 		add_child(item)
-	
+
 	for item in ItemCreator.create_all(Craft):
 		$Craft.position.x += 25
 		item.position = $Craft.position
 		add_child(item)
-	
+
 	for item in ItemCreator.create_all(Gear):
 		$Gear.position.x += 25
 		item.position = $Gear.position
 		add_child(item)
-	
+
 	for item in ItemCreator.create_all(Knife):
 		$Knife.position.x += 25
 		item.position = $Knife.position
 		add_child(item)
-	
+
 	for item in ItemCreator.create_all(MeleeWeapon):
 		$MeleeWeapon.position.x += 25
 		item.position = $MeleeWeapon.position
 		add_child(item)
-	
+
 	for item in ItemCreator.create_all(Pistol):
 		$Pistol.position.x += 25
 		item.position = $Pistol.position
 		add_child(item)
-	
+
 	for item in ItemCreator.create_all(MainWeapon):
 		$MainWeapon.position.x += 25
 		item.position = $MainWeapon.position
@@ -46,33 +48,33 @@ func stack_test():
 	$ItemPosition.position.x += 20
 	item.position = $ItemPosition.position
 	add_child(item)
-	
+
 	item = ItemCreator.create_item(NumberItem, 0)
 	$ItemPosition.position.x += 20
 	item.position = $ItemPosition.position
 	add_child(item)
-	
+
 	item = ItemCreator.create_item(NumberItem, 0)
 	item.number = 30
 	$ItemPosition.position.x += 20
 	item.position = $ItemPosition.position
 	add_child(item)
-	
+
 	item = ItemCreator.create_item(Craft, 85)
 	$ItemPosition.position.x += 20
 	item.position = $ItemPosition.position
 	add_child(item)
-	
+
 	item = ItemCreator.create_item(Knife, 11)
 	$ItemPosition.position.x += 200
 	item.position = $ItemPosition.position
 	add_child(item)
-	
+
 	item = ItemCreator.create_item(Gear, 0)
 	$ItemPosition.position.x += 200
 	item.position = $EquipmentPosition.position
 	add_child(item)
-	
+
 	item = ItemCreator.create_item(Gear, 1)
 	$EquipmentPosition.position.x += 200
 	item.position = $EquipmentPosition.position
--- test/test.gd
+++ test/test.gd
@@ -1,8 +1,20 @@
-extends Control
+extends Node2D
 
+
+var offset := 165
+
+var type := Pistol
 
 func _ready():
-	pass
+	for key in type.RES_TABLE.keys():
+		var res := type.RES_TABLE.get(key) as ItemResource
+		res.id += offset
+		ResourceSaver.save(res)
+	print_info()
 
-func _gui_input(event):
-	print(Rect2($Control/ColorRect.get_rect()).has_point(event.position))
+func print_info():
+	print('{')
+	for key in type.RES_TABLE.keys():
+		var res := type.RES_TABLE.get(key) as ItemResource
+		print('	%d: preload(\'%s\'),' % [res.id, res.resource_path])
+	print('}')
