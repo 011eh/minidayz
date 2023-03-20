29bb10754a1bdd8760b761f2604703dde4de8f61
011eh <1395663435@qq.com>
2023-03-20 21:45:00 +0800

    测试

--- test/test.gd
+++ test/test.gd
@@ -1,13 +1,5 @@
 extends Node2D
 
 
-var offset := 165
-
-var type := Pistol
-
 func _ready():
-	var item1 := ItemCreator.create_item_from_id(13)
-	var item2 := ItemCreator.create_item_from_id(17)
-	item2.number = 1
-	var optoins := ItemActionTable.create_crafting_options(item1, item2)
-	optoins.front().action.call()
+    pass
