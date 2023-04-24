3f0ad106558b4b183a0e426024e9bb553c893868
011eh <1395663435@qq.com>
2023-04-24 17:59:29 +0800

    环境：道路绘制

--- environment/ground.gd
+++ environment/ground.gd
@@ -2,6 +2,27 @@ extends Node2D
 
 
 enum Tile {TERRAIN, DECORATION}
+enum BlockType {
+	GRASSLAND,
+	LAKE,
+	VILLAGE,
+	CITY,
+	HOSPITAL,
+	FIRE_STATION,
+	NATO_MILITARY_CAMP,
+	WARSAW_PACT_MILITARY_CAMP,
+	SHOOTING_RANGE,
+	CAMP_SITE,
+	GAS_STATION_X,
+	GAS_STATION_Y,
+	ROAD_X,
+	ROAD_X_NEG_Y,
+	ROAD_X_POS_Y,
+	ROAD_Y,
+	ROAD_Y_NEG_X,
+	ROAD_Y_POS_X,
+	CROSSROAD,
+}
 
 
 const SIZE_IN_CELLS := 14
@@ -16,6 +37,10 @@ var decoration := %Decoration
 
 
 func _ready():
+	init()
+	pass
+
+func init():
 	var location_number := 25
 	var i := 0
 	var location_block: Array[Vector2i]
@@ -26,10 +51,15 @@ func _ready():
 			continue
 		i += 1
 		location_block.append(p)
-	var paths_block_dict := create_path_block(location_block)
-	for x in range(MAP_AREA_SIZE * SIZE_IN_CELLS):
-		for y in range(MAP_AREA_SIZE * SIZE_IN_CELLS):
-			terrain.set_cell(0, Vector2i(x, y), 0, pick_random_tile(Tile.TERRAIN, 0))
+	var block_dict := create_path_block(location_block)
+	
+#	for x in range(MAP_AREA_SIZE * SIZE_IN_CELLS):
+#		for y in range(MAP_AREA_SIZE * SIZE_IN_CELLS):
+#			terrain.set_cell(0, Vector2i(x, y), 0, pick_random_tile(Tile.TERRAIN, 0))
+	
+	var cells := get_cells_in_path_block({Vector2i(1,1):BlockType.ROAD_X_POS_Y})
+	print(cells)
+	terr_set_terrain(0, cells, 0, 1, false)
 #	terr_set_terrain(2, get_cells_in_blocks(location_block, SIZE_IN_CELLS), 0, 3)
 #	terr_set_terrain(1, get_cells_in_blocks(paths_block_dict.keys(), SIZE_IN_CELLS), 0, 2)
 
@@ -76,29 +106,52 @@ func get_cells_in_blocks(blocks: Array, area_size: int) -> Array[Vector2i]:
 		cells.append_array(array)
 	return cells
 
+func get_cells_in_path_block(path_block_dict: Dictionary) -> Array[Vector2i]:
+	var cells: Array[Vector2i]
+	for v in path_block_dict.keys():
+		var type := path_block_dict.get(v) as BlockType
+		if type == BlockType.CROSSROAD:
+			pass
+		elif type in range(BlockType.ROAD_X, BlockType.ROAD_X_POS_Y + 1):
+			for x in range(SIZE_IN_CELLS):
+				for y in range(4):
+					cells.append(v * SIZE_IN_CELLS + Vector2i(x, y + 5))
+			if type == BlockType.ROAD_X:
+				continue
+			
+			var offset := 0 if type == BlockType.ROAD_X_NEG_Y else 9
+			for x in range(4):
+				for y in range(5):
+					cells.append(v * SIZE_IN_CELLS + Vector2i(x + 5, y + offset))
+			return cells
+		else:
+			return cells
+	return cells
+
 func terr_set_terrain(layer: int, cells: Array[Vector2i], terrain_set: int, terr: int, with_grass_layer: bool = true):
 	terrain.set_cells_terrain_connect(layer, cells, terrain_set, terr, false)
 	if with_grass_layer:
 		terr_set_terrain(0, cells, 0, 0, false)
 
-func terr_get_path_block_type(axis: String, coords: Vector2i, paths: Dictionary, locations := [] as Array[Vector2i]) -> Vector2i:
+func terr_get_path_block_type(axis: String, coords: Vector2i, paths: Dictionary, locations := [] as Array[Vector2i]) \
+	-> BlockType:
 	if paths.has(coords):
-		return Vector2i(2, 2)
+		return BlockType.CROSSROAD
 	else:
-		var v: Vector2
+		var type: BlockType
 		if axis == 'x':
-			v = Vector2i(0, 2)
+			type = BlockType.ROAD_Y
 			if locations.has(Vector2i(coords.x - 1, coords.y)):
-					v.x = -1
+					type = BlockType.ROAD_Y_NEG_X
 			if locations.has(Vector2i(coords.x + 1, coords.y)):
-					v.x = 1 if v.x == 0 else 2
+					type = BlockType.ROAD_Y_POS_X
 		else:
-			v = Vector2i(2, 0)
+			type = BlockType.ROAD_X
 			if locations.has(Vector2i(coords.x, coords.y - 1)):
-					v.y = -1
+					type = BlockType.ROAD_X_NEG_Y
 			if locations.has(Vector2i(coords.x, coords.y + 1)):
-					v.y = 1 if v.y == 0 else 2
-		return v
+					type = BlockType.ROAD_X_POS_Y
+		return type
 
 # https://github.com/godotengine/godot/blob/b1c18f807bfa3ad2e807ad920bc5f55b5e4061bd/editor/plugins/tiles/tile_map_editor.cpp
 func pick_random_tile(tile: Tile, pattern: int, scattering: float = 0) -> Vector2i:
@@ -117,36 +170,3 @@ func pick_random_tile(tile: Tile, pattern: int, scattering: float = 0) -> Vector
 		if current >= rand:
 			return coords
 	return Vector2i(-1, -1)
-
-class BlockData:
-
-
-	enum Type {
-		GRASSLAND,
-		LAKE,
-		VILLAGE,
-		CITY,
-		HOSPITAL,
-		FIRE_STATION,
-		NATO_MILITARY_CAMP,
-		WARSAW_PACT_MILITARY_CAMP,
-		SHOOTING_RANGE,
-		CAMP_SITE,
-		GAS_STATION_X,
-		GAS_STATION_Y,
-		ROAD_X,
-		ROAD_X_neg_Y,
-		ROAD_X_pos_Y,
-		ROAD_Y,
-		ROAD_Y_neg_X,
-		ROAD_Y_pos_X
-	}
-	
-	
-	var coords: Vector2i
-	var type: Type
-	
-	
-	func _init(coords: Vector2i, type: Type):
-		self.coords = coords
-		self.type = type
