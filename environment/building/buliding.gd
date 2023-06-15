3b51be83816793f43739fa6ff089994a5f45567f
011eh <1395663435@qq.com>
2023-06-15 17:58:02 +0800

    环境：创建医院建筑，透明效果处理

--- environment/building/buliding.gd
+++ environment/building/buliding.gd
@@ -4,35 +4,39 @@ class_name Building
 
 
 var player: Player
-var camera_entered := false
-@onready
-var area := %DetectionArea as Area2D
-
+var interior_zone: Rect2
 
 func _ready():
-	
 	y_sort_enabled = true
-	area.area_entered.connect(func(area: Area2D) -> void:
-		camera_entered = true
-		player = area.owner
+	
+	var origin_size := %Inside.get_rect().size as Vector2
+	interior_zone = Rect2(%Inside.position, origin_size + %Inside.offset)
+	
+	%DetectionArea.body_entered.connect(func(player: Node2D) -> void:
+		self.player = player
 	)
-	area.area_exited.connect(func(area: Area2D) -> void:
-		camera_entered = false
+	%DetectionArea.body_exited.connect(func(player: Node2D) -> void:
 		change_transparent(1)
+		self.player = null
 	)
 
 func _process(delta):
-	if camera_entered:
+	if is_instance_valid(player):
+		if in_building(player.position):
+			change_transparent(0, true)
+			return
+		
 		var player_y := player.position.y
 		if player_y < %Inside.position.y:
 			change_transparent(0.2)
 			return
-		if player_y >= %Inside.position.y and player_y < %Outside.position.y:
-			change_transparent(1)
-			return
+		change_transparent(1)
 
 func change_transparent(value: float, only_outside: bool = false) -> void:
 	if only_outside:
 		%Outside.modulate.a = value
 	else:
 		get_tree().call_group('building_sprites', 'set_indexed', 'modulate:a', value)
+
+func in_building(position: Vector2) -> bool:
+	return interior_zone.has_point(position)
