80d7984bf6eb884385588cc9ccabc1aea6cac7cd
011eh <1395663435@qq.com>
2023-06-26 17:49:27 +0800

    环境：建筑透明处理

--- environment/building/buliding.gd
+++ environment/building/buliding.gd
@@ -10,16 +10,18 @@ var interior_zone := Rect2(%Inside.global_position, %Inside.get_rect().size + %I
 func _ready():
 	y_sort_enabled = true
 	
-	var outside_rect := Rect2(%Outside.global_position + %Outside.offset, %Outside.get_rect().size)
+	%Inside.add_to_group('building_sprites_%d' % get_instance_id())
+	%Outside.add_to_group('building_sprites_%d' % get_instance_id())
+	
+	var outside_rect := Rect2(%Outside.position + %Outside.offset, %Outside.get_rect().size)
 	%DetectionArea/CollisionShape2D.shape.size = outside_rect.size + Vector2(
 		ProjectSettings.get_setting('display/window/size/viewport_width'),
 		ProjectSettings.get_setting('display/window/size/viewport_height') / 2
 	)
 	%DetectionArea/CollisionShape2D.position = Vector2(
 		outside_rect.get_center().x,
-		# todo
-		outside_rect.get_center().y - %DetectionArea/CollisionShape2D.shape.size.y / 2)
-	
+		outside_rect.get_center().y - (%DetectionArea/CollisionShape2D.shape.size.y - outside_rect.size.y) / 2
+		)
 	%DetectionArea.body_entered.connect(func(player: Node2D) -> void:
 		self.player = player
 	)
@@ -44,7 +46,7 @@ func change_transparent(value: float, only_outside: bool = false) -> void:
 	if only_outside:
 		%Outside.modulate.a = value
 	else:
-		get_tree().call_group('building_sprites', 'set_indexed', 'modulate:a', value)
+		get_tree().call_group('building_sprites_%d' % get_instance_id(), 'set_indexed', 'modulate:a', value)
 
 func in_building(position: Vector2) -> bool:
 	return interior_zone.has_point(position)
