075cb90b7bb994b0d5838e0a2bf8c2619ba86c74
011eh <1395663435@qq.com>
2023-11-03 17:59:45 +0800

    环境：建筑修改、添加门实例

--- environment/building/door.gd
+++ environment/building/door.gd
@@ -1,21 +1,25 @@
 extends Sprite2D
 
 
+var tween: Tween
+var is_opened := false
 @export 
 var double_door := false
 
-var tween: Tween
-
 
 func _ready():
-	tween = create_tween()
-	tween.tween_property(%Door1, 'rotation', deg_to_rad(90), 0)
-	if double_door:
-		tween.tween_property(%Door2, 'rotation', deg_to_rad(-90), 0)
-	else:
+	if not double_door:
 		%Door2.queue_free()
-	tween.play()
-	tween.stop()
+	
+	$InteractionArea.interaction_inputted.connect(func():
+		play_door_tween(is_opened)
+		is_opened = not is_opened
+	)
 
-func _process(delta):
-	pass
+func play_door_tween(is_opened: bool) -> void:
+	tween = create_tween()
+	tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
+	if double_door:
+		tween.tween_property(%Door2, 'rotation', deg_to_rad(0 if is_opened else -90), 0)
+	tween.play()
+
