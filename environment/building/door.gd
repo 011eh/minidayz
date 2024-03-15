9196ef1a5199970e639b05db7ce19c83ee966d4c
011eh <o11eh@foxmail.com>
2024-03-15 18:20:10 +0800

    素材: 建筑门场景

--- environment/building/door.gd
+++ environment/building/door.gd
@@ -3,23 +3,20 @@ extends Sprite2D
 
 var tween: Tween
 var is_opened := false
-@export 
-var double_door := false
+var tween_callable: Callable
 
 
 func _ready():
-	if not double_door:
-		%Door2.queue_free()
-	
-	$InteractionArea.interaction_inputted.connect(func():
-		play_door_tween(is_opened)
-		is_opened = not is_opened
-	)
-
-func play_door_tween(is_opened: bool) -> void:
-	tween = create_tween()
-	tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
-	if double_door:
-		tween.tween_property(%Door2, 'rotation', deg_to_rad(0 if is_opened else -90), 0)
-	tween.play()
-
+	if %StaticBody2D.get_child_count() > 1:
+		$InteractionArea.interaction_inputted.connect(func():
+			tween = create_tween()
+			tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
+			tween.play()
+		)
+	else:
+		$InteractionArea.interaction_inputted.connect(func():
+			tween = create_tween()
+			tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
+			tween.tween_property(%Door2, 'rotation', deg_to_rad(0 if is_opened else -90), 0)
+			tween.play()
+		)
