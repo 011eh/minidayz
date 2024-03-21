68a0f324cfe68e13ed2389b71dd80e8a95ccb369
011eh <o11eh@foxmail.com>
2024-03-21 18:15:17 +0800

    环境：给建筑添加“门”场景

--- environment/building/door.gd
+++ environment/building/door.gd
@@ -7,10 +7,13 @@ var tween_callable: Callable
 
 
 func _ready():
-	if %StaticBody2D.get_child_count() > 1:
+	print(%StaticBody2D.get_child_count())
+	if %StaticBody2D.get_child_count() == 1:
 		$InteractionArea.interaction_inputted.connect(func():
 			tween = create_tween()
 			tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
+			tween.tween_property(self, 'frame', 0 if is_opened else 1, 0)
+			is_opened = not is_opened
 			tween.play()
 		)
 	else:
@@ -18,5 +21,7 @@ func _ready():
 			tween = create_tween()
 			tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
 			tween.tween_property(%Door2, 'rotation', deg_to_rad(0 if is_opened else -90), 0)
+			tween.tween_property(self, 'frame', 1 if is_opened else 0, 0)
+			is_opened = not is_opened
 			tween.play()
 		)
