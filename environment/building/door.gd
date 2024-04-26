34c486e9a5744bfee0bebfc15182bd75e34fb774
011eh <o11eh@foxmail.com>
2024-04-26 19:06:49 +0800

    环境：建筑添加门场景，门动画逻辑修改

--- environment/building/door.gd
+++ environment/building/door.gd
@@ -16,19 +16,19 @@ func _ready():
 	
 	if %StaticBody2D.get_child_count() == 1:
 		$InteractionArea.interaction_inputted.connect(func():
+			is_opened = not is_opened
 			tween = create_tween()
 			tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
-			tween.tween_property(self, 'frame', 0 if is_opened else 1, 0)
-			is_opened = not is_opened
+			tween.tween_property(self, 'frame', 1 if is_opened else 0, 0)
 			tween.play()
 		)
 	else:
 		# double door
 		$InteractionArea.interaction_inputted.connect(func():
+			is_opened = not is_opened
 			tween = create_tween()
 			tween.tween_property(%Door1, 'rotation', deg_to_rad(0 if is_opened else 90), 0)
 			tween.tween_property(%Door2, 'rotation', deg_to_rad(0 if is_opened else -90), 0)
 			tween.tween_property(self, 'frame', 1 if is_opened else 0, 0)
-			is_opened = not is_opened
 			tween.play()
 		)
